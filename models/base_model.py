52954e1539571ee69a128150e7753e5ce25fdaa3
kayodegigz <kayogunrinde@gmail.com>
2022-08-04 03:05:35 +0100

    updating the base class

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,15 @@
+#!/usr/bin/python3
+"""
+Module contains the base class that every other class will
+inherit from
+"""
 from datetime import datetime
 from uuid import uuid4
 
+
 class BaseModel ():
     def __init__(self, *args, **kwargs):
+        """initialises instance attribute values"""
         if kwargs:
             for key, value in kwargs:
                 if key == "created_at":
@@ -20,6 +27,22 @@ class BaseModel ():
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
 
+    def __str__(self):
+        """returns the specified string representation"""
+        return "[{}] ({}) {}".format(__class__.name, self.id, self.__dict__)
 
+    def save(self):
+        """changes the updated_at time so we know when last the instance was modified"""
+        self.updated_at = datetime.datetime.now()
 
+    def to_dict(self):
+        """returns a dictionary representation of all attrs and the class name"""
+        new_dict = {}
+        for key, value in self.__dict__.items():
+            new_dict[key] = value
 
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        new_dict["__class__"] = type(self).__name__
+
+        return new_dict
