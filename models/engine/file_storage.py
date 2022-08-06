e8abacc5001d64fe4923d1a3c900be783b9ff4c9
kayodegigz <kayogunrinde@gmail.com>
2022-08-06 02:08:50 +0100

    defining the methods in the file storage engine class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,11 +4,22 @@ JSON instantiation class
 """
 
 import json
+import os
 from models.user import User
 
 class FileStorage:
+    """"""
     __file_path = ""
     __objects = {}
+
+    def all(self):
+        """"""
+        return self.__objects
+
+    def new(self, obj):
+        """"""
+        obj_dict_key = f"{type(obj).__name__}.{obj.id}"
+        self.__objects[obj_dict_key] = self.__dict__
 
     def save(self):
         """
@@ -21,4 +32,7 @@ class FileStorage:
 
         with open(self.__file_path, 'w') as file:
             json.dump(new_obj, file)
+
+    def reload(self):
+        if os.path.exists(self.__file_path):
 
