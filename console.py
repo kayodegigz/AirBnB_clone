5dbdadfe0b160785ca1a7d66ce67ff6a3fc5dfe4
kayodegigz <kayogunrinde@gmail.com>
2022-08-08 01:21:42 +0100

    changes made to console.py file, additional methods added

--- console.py
+++ console.py
@@ -4,6 +4,9 @@ Entry point of the command interpreter
 """
 
 import cmd
+from models.base_model import BaseModel
+from models import storage  # in the init file
+
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -22,6 +25,50 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do nothing"""
         pass
+
+    def do_create(self, line):
+        """creates a new instance of basemodel class"""
+        line_list = line.split()
+        if len(line_list) < 1:
+            print("** class name missing **")
+        if line_list[0] != "BaseModel": #we'll have to refactor and make this dynamic
+            print("** class doesn't exist **")
+       new_inst = BaseModel()
+       new_inst.save()
+       print(new_inst.obj)
+
+    def do_show(self, line):
+        line_list = line.split()
+        if len(line_list) < 1:
+            print("** class name missing **")
+        if line_list[0] != "BaseModel":  # this also has to be dynamic
+            print("** class doesn't exist **")
+        if len(line_list) < 2:
+            print("** instance id missing **")
+        obj_key = ".".join(line_list)
+        if obj_key not in storage.all():  # this too
+            print("** no instance found **")
+        print(storage.all()[obj_key])
+
+    def do_destroy(self, line):
+        line_list = line.split()
+        if len(line_list) < 1:
+            print("** class name missing **")
+        if line_list[0] != "BaseModel":  # this
+            print("** class doesn't exist **")
+        if len(line_list) < 2:
+            print("** instance id missing **")
+        obj_key = ".".join(line_list)
+        if obj_key not in storage.all():  # this too
+            print("** no instance found **")
+        del storage.all()[obj_key]
+
+    def do_all(self, line):
+        line_list = list.split()
+        if len_list[0] != "BaseModel":
+            print("** class doesn't exist **")
+
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
