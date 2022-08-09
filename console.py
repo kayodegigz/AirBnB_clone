207d820570745d549e50008a578f115cdf482410
kayodegigz <kayogunrinde@gmail.com>
2022-08-09 21:13:15 +0100

    changes to the console.py

--- console.py
+++ console.py
@@ -40,7 +40,7 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
         new_inst = BaseModel()
         new_inst.save()
-        print(new_inst.obj)
+        print(new_inst.id)
 
     def do_show(self, line):
         """Print string representation: name and id"""
@@ -65,24 +65,28 @@ class HBNBCommand(cmd.Cmd):
         line_list = line.split()
         if len(line_list) < 1:
             print("** class name missing **")
+            return
         if line_list[0] != "BaseModel":  # this
             print("** class doesn't exist **")
+            return
         if len(line_list) < 2:
             print("** instance id missing **")
+            return
         obj_key = ".".join(line_list)
-        if obj_key not in storage.all():  # this too
+        if obj_key not in storage.all().keys():  # this too
             print("** no instance found **")
+            return
         del storage.all()[obj_key]
         storage.save()
 
     def do_all(self, line):
         """Print all objects or all objects of specified class"""
         line_list = line.split()
-        if line_list[0] != "BaseModel":  # has to be dynamic
-            print("** class doesn't exist **")
-            return
         if len(line_list) == 0:  # if no arg is passed to all command
             print([str(v) for v in storage.all().values()])
+        elif line_list[0] != "BaseModel":  # has to be dynamic
+            print("** class doesn't exist **")
+            return
         elif len(line_list) > 0:
             print([str(v) for v in storage.all().values()
                   if type(v).__name__ == line_list[0]])
@@ -92,23 +96,29 @@ class HBNBCommand(cmd.Cmd):
         line_list = line.split()
         if len(line_list) < 1:
             print("** class name missing **")
+            return
         if line_list[0] != "BaseModel":
             print("** class doesn't exist **")
+            return
         if len(line_list) < 2:
             print("** instance id missing **")
+            return
         key = f"{line_list[0]}.{line_list[1]}"
         if key not in storage.all().keys():
             print("** no instance found **")
+            return
         if len(line_list) < 3:
             print("** attribute name missing **")
+            return
         if len(line_list) < 4:
             print("** value missing **")
+            return
         if isinstance(eval(line_list[3]), int):  # strip off str ndcheck if int
             line_list[3] = int(line_list[3])
         elif isinstance(eval(line_list[3]), float):  # or if float
             line_list[3] = float(line_list[3])
         else:
-            line_list[3] = str(line_list[3])
+            line_list[3] = line_list[3]
 
         obj = storage.all()[key]  # check for d particular inst
         setattr(obj, line_list[2], line_list[3])
